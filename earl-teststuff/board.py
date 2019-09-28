e04ee932dab8e5e34355d11b8f31f3c91278e707
Pandaromin <earlaromin@gmail.com>
2019-09-28 14:58:01 -0400

    Helper functions for move

--- earl-teststuff/board.py
+++ earl-teststuff/board.py
@@ -1,119 +1,159 @@
-import numpy as np 
+import numpy as np
 import config
+
 
 class Board:
 
-	def __init__(self):	
-		self.LETTERS = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-		self.used_tiles = []
-		rows = config.BOARDHEIGHT
-		columns = config.BOARDWIDTH 
-		self.board = np.zeros(shape=(rows,columns))
-		self.board = self.board.astype(int)
-		self.winner_found = False
-		self.turnCounter = 30
+    def __init__(self):
+        self.LETTERS = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+        self.used_tiles = []
+        rows = config.BOARDHEIGHT
+        columns = config.BOARDWIDTH
+        self.board = np.zeros(shape=(rows, columns))
+        self.board = self.board.astype(int)
+        self.winner_found = False
+        self.turnCounter = 30
 
-	def displayBoard(self,board):
-		for y in range(config.BOARDHEIGHT):
-			row = str(config.BOARDHEIGHT-y)+" |"
-			for  x in range(config.BOARDWIDTH):
-				#TEMPORARY, REPLACE 6 WITH 'X' FOR TESTING
-				cell = ""
-				if self.board[y][x] == 6:
-					cell = " X |"
-				elif self.board[y][x] == 9:
-					cell = " O |"
-				else :
-					cell = " - |"
+    def displayBoard(self, board):
+        for y in range(config.BOARDHEIGHT):
+            row = str(config.BOARDHEIGHT - y) + " |"
+            for x in range(config.BOARDWIDTH):
+                # TEMPORARY, REPLACE 6 WITH 'X' FOR TESTING
+                cell = ""
+                if self.board[y][x] == 6:
+                    cell = " X |"
+                elif self.board[y][x] == 9:
+                    cell = " O |"
+                else:
+                    cell = " - |"
 
-				row += cell
-			print(row)
-		bottom_text = "   "
-		for x in range(config.BOARDWIDTH):
-			bottom_text += " " + self.LETTERS[x] + "  "
-		print(bottom_text)
-		print("Turns left: " + str(self.turnCounter))
+                row += cell
+            print(row)
+        bottom_text = "   "
+        for x in range(config.BOARDWIDTH):
+            bottom_text += " " + self.LETTERS[x] + "  "
+        print(bottom_text)
+        print("Turns left: " + str(self.turnCounter))
 
-	#Position should be <LETTERNUMBER> e.g 'H2'
-	def setTile(self,entry,position):
-		valid_turn = False 
-		if(((position,6) in self.used_tiles) or ((position,6) in self.used_tiles)):
-			print("Piece already in tile, please try again.")
-			return valid_turn
-		
-		row = self.LETTERS.index(position[0])
-		column = config.BOARDHEIGHT - int(position[1])
-		self.board[column][row] = entry
-		
-		self.used_tiles.append((position,entry)) #to make checking easier
+    # Position should be <LETTERNUMBER> e.g 'H2'
+    def setTile(self, entry, position):
+        valid_turn = False
+        if(((position, 6) in self.used_tiles) or ((position, 6) in self.used_tiles)):
+            print("Piece already in tile, please try again.")
+            return valid_turn
 
-		valid_turn = True
+        row = self.LETTERS.index(position[0])
+        column = config.BOARDHEIGHT - int(position[1])
+        self.board[column][row] = entry
 
-		self.turnCounter -= 1
+        self.used_tiles.append((position, entry))  # to make checking easier
 
-		return valid_turn
-	def moveTile(self,entry,previous_position,new_position):
-		valid_move = False
-		if((previous_position,entry) in self.used_tiles):
-			self.used_tiles.remove((previous_position,entry))
-			row = self.LETTERS.index(previous_position[0])
-			column = config.BOARDHEIGHT - int(previous_position[1])
-			self.board[column][row] = 0
-		else:
-			print("Either entry is wrong or original position")
-			return valid_move
-		return self.setTile(entry,new_position)
+        valid_turn = True
 
-	
-	def checkTile(self,position):
-		row = self.LETTERS.index(position[0].upper())
-		column = config.BOARDHEIGHT - int(position[1])
-		symbol = self.board[column][row]
-		#print("Symbol: " + str(symbol))
-		x_drawn = False
-		crossed = False
-		#OUT OF BOUNDS
-		if(row + 2 >= config.BOARDWIDTH):
-			#print("Nothing on right")
-			return False
-		if(column +2 >= config.BOARDHEIGHT):
-			#print("Nothing below")
-			return False
-		#EMPTY CELL
-		if symbol==0:
-			return False
-		
-	    #Check if X is drawn
-		if (self.board[column ][row + 2] == symbol #right      
-		and self.board[column +2][row] == symbol   #below  
-		and self.board[column + 2][row +2] == symbol  #bottom right
-		and self.board[column + 1][row + 1] == symbol):    #middle
-			
-			x_drawn = True
-		
-	    #Check for strikethrough
-		midleft = self.board[column + 1][row]
-		midright = self.board[column + 1][row + 2]
-		
-		if((midleft != 0 and midleft != symbol)
-		and (midright !=0 and midright != symbol)):
-			crossed = True
+        self.turnCounter -= 1
 
-		self.winner_found = (x_drawn and not crossed)
-		return symbol
+        return valid_turn
 
-	def checkWinner(self):
-		for tile in self.used_tiles:
-			result = self.checkTile(tile[0])
-			#print(tile + ": " + str(self.winner_found))
-			if(self.winner_found):
-				winner = ""
-				if(result == 6):
-					winner = 'X'
-				elif(result == 9):
-					winner = 'O'
-				print("Winner: " + winner)
-				return winner
+    def moveTile(self, entry, previous_position, new_position):
+        valid_move = False
+        if((previous_position, entry) in self.used_tiles):
+            self.used_tiles.remove((previous_position, entry))
+            row = self.LETTERS.index(previous_position[0])
+            column = config.BOARDHEIGHT - int(previous_position[1])
+            self.board[column][row] = 0
+        else:
+            print("Either entry is wrong or original position")
+            return valid_move
+        return self.setTile(entry, new_position)
 
-			if (self.turnCounter == 0):
-				print("Draw")
+    # Will return a list of possible positions.
+    # And show it visually too.
+    def showNeighbours(self, position):
+        row = self.LETTERS.index(position[0].upper())
+        column = config.BOARDHEIGHT - int(position[1])
+
+        open_cell_list = []
+
+        for y in range(-1, 2):
+            relative_column = column + y
+            if(relative_column >= config.BOARDHEIGHT or
+                    relative_column < 0):
+                continue
+            row_text = str(config.BOARDHEIGHT - (relative_column)) + " |"
+            for x in range(-1, 2):
+                relative_row = row + x
+                if(relative_row >= config.BOARDWIDTH or
+                        relative_row < 0):
+                    continue
+                if y == 0 and x == 0:
+                    cell = " ? |"
+                elif self.board[relative_column][relative_row] == 6:
+                    cell = " X |"
+                elif self.board[relative_column][relative_row] == 9:
+                    cell = " O |"
+                else:
+                    cell = " - |"
+                    open_cell_list.append(self.LETTERS[relative_row] + str(config.BOARDHEIGHT - relative_column))
+                row_text += cell
+
+            print(row_text)
+        bottom_text = "   "
+        for x in range(-1, 2):
+            bottom_text += " " + self.LETTERS[row + x] + "  "
+        print(bottom_text)
+        open_cell_list.sort()
+        return open_cell_list
+
+    def checkTile(self, position):
+        row = self.LETTERS.index(position[0].upper())
+        column = config.BOARDHEIGHT - int(position[1])
+        symbol = self.board[column][row]
+        # print("Symbol: " + str(symbol))
+        x_drawn = False
+        crossed = False
+        # OUT OF BOUNDS
+        if(row + 2 >= config.BOARDWIDTH):
+            # print("Nothing on right")
+            return False
+        if(column + 2 >= config.BOARDHEIGHT):
+            # print("Nothing below")
+            return False
+        # EMPTY CELL
+        if symbol == 0:
+            return False
+
+# Check if X is drawn
+        if (self.board[column][row + 2] == symbol and  # right
+            self.board[column + 2][row] == symbol and  # below
+            self.board[column + 2][row + 2] == symbol and  # bottom right
+                self.board[column + 1][row + 1] == symbol):  # middle
+
+            x_drawn = True
+
+# Check for strikethrough
+        midleft = self.board[column + 1][row]
+        midright = self.board[column + 1][row + 2]
+
+        if((midleft != 0 and midleft != symbol) and
+                (midright != 0 and midright != symbol)):
+            crossed = True
+
+        self.winner_found = (x_drawn and not crossed)
+        return symbol
+
+    def checkWinner(self):
+        for tile in self.used_tiles:
+            result = self.checkTile(tile[0])
+            # print(tile + ": " + str(self.winner_found))
+            if(self.winner_found):
+                winner = ""
+                if(result == 6):
+                    winner = 'X'
+                elif(result == 9):
+                    winner = 'O'
+                print("Winner: " + winner)
+                return winner
+
+            if (self.turnCounter == 0):
+                print("Draw")
--- earl-teststuff/e-main2.py
+++ earl-teststuff/e-main2.py
@@ -1,23 +1,24 @@
 import board
 
 b = board.Board()
-b.setTile(6,"H3")
+b.setTile(6, "H3")
 
-b.setTile(6,"G2")
-b.setTile(6,"G4")
-b.setTile(6,"I4")
+b.setTile(6, "G2")
+b.setTile(6, "G4")
+b.setTile(6, "I4")
 
-b.setTile(6,"I2")
+b.setTile(6, "I2")
 
-#b.setTile(9,"I3")
-b.setTile(9,"G3")
-position = "G4"
-#board[column ][row + 2] = 9
+# b.setTile(9,"I3")
+b.setTile(9, "G3")
+# board[column ][row + 2] = 9
 
 b.displayBoard(board)
 b.checkWinner()
 print(b.used_tiles)
 
-b.moveTile(6,"G2","A6")
+b.moveTile(6, "G2", "A6")
 b.displayBoard(board)
 
+available_cells = b.showNeighbours("H3")
+print("Can move to: " + str(available_cells))
