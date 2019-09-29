51155f733526d303ecd0414ee7616d6000a41533
Pandaromin <earlaromin@gmail.com>
2019-09-29 14:06:49 -0400

    Further testing
    First tests involving a 10x12 board. Both placements and movements.
    Depth: 2
    Not alternating players yet.
    Without printing tree: 1.3s
    With tree printing: 2.0s
    Number of nodes: 21199

--- earl-teststuff/Deliverable 2 tests/config.py
+++ earl-teststuff/Deliverable 2 tests/config.py
@@ -9,7 +9,7 @@ go there for the sake of simplicity and clarity.
 # BELOW ARE CONFIG FOR OUR GAME SCREEN
 # ==================================================
 BOARDWIDTH = 12 # how many spaces wide the board is
-BOARDHEIGHT = 6 # how many spaces tall the board is
+BOARDHEIGHT = 10 # how many spaces tall the board is
 
 DIFFICULTY = 2 # how many moves to look ahead.
 SPACESIZE = 50 # size of the tokens and individual board spaces in pixels.
--- earl-teststuff/Deliverable 2 tests/treeTests.py
+++ earl-teststuff/Deliverable 2 tests/treeTests.py
@@ -3,11 +3,10 @@ import board
 import copy
 import numpy as np
 
-
+#nodecount = 1
 def setMoveNodes(starting_node, token, turncount, steps):
+    #global nodecount
 
-#    if steps == 0:
-#        return
     base_board = board.Board()
     base_board.setBoardToState(starting_node.name, turncount)
 
@@ -18,22 +17,21 @@ def setMoveNodes(starting_node, token, turncount, steps):
             temp_board = board.Board()
             temp_board.setBoardToState(starting_node.name, turncount)
             temp_board.moveTile(token, used[0], neighbour)
-            child = Node(temp_board.used_tiles, parent=starting_node)
-            
-            #setMoveNodes(child, token, temp_board.turnCounter, steps - 1)
+            child = Node(temp_board.used_tiles.copy(), parent=starting_node)
+           # nodecount+=1
 
 
 def setPlaceNodes(starting_node, token, turncount, steps):
-#    if steps == 0:
-#        return
+
+    #global nodecount
     base_board = board.Board()
     base_board.setBoardToState(starting_node.name, turncount)
 
     for ix,iy in np.ndindex(base_board.board.shape):
     	if(base_board.aiSetTile(token, ix, iy)):
     		child = Node(base_board.used_tiles.copy(), parent=starting_node)
-    		#setPlaceNodes(child, token, copy.copy(base_board.turnCounter), steps-1)
     		base_board.aiRemoveTile(ix, iy)
+    		#nodecount+=1
 
 
 def generateSearchSpace(starting_node,token,turncount,steps):
@@ -48,11 +46,6 @@ def generateSearchSpace(starting_node,token,turncount,steps):
 
 
 
-#for ix,iy in np.ndindex(b.board.shape):
-#	if(b.aiSetTile(9, ix, iy)):
-#		b.displayBoard()
-#		b.aiRemoveTile(ix, iy)
-#		print(b.used_tiles)
 
 
 b = board.Board()
@@ -67,24 +60,19 @@ b.setTile(9, "I3")
 # b.setTile(9,"I3")
 # b.setTile(9, "G3")
 
-b.displayBoard()
+#b.displayBoard()
 
 init = Node(b.used_tiles)
 
 
-# for neighbours in b.showNeighbours("I3"):
-#    temp_board = board.Board()
-#    temp_board.setBoardToState(b.used_tiles, b.turnCounter)
-#    temp_board.moveTile(9, "I3", neighbours)
-#
-#    child = Node(temp_board.used_tiles.copy(), parent=init)
+
 generateSearchSpace(init, 6, b.turnCounter, 2)
-#setMoveNodes(init, 6, b.turnCounter, 2)
-for pre, fill, node in RenderTree(init):
-    print("%s%s" % (pre, node.name))
-
-# sets the game state 1 level below if we choose to move a token
-
-b.displayBoard()
 
 
+#for pre, fill, node in RenderTree(init):
+#    print("%s%s" % (pre, node.name))
+
+
+
+
+#print(nodecount)
